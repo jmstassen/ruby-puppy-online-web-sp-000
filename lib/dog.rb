4a2a95569a8574e4ab3b4e22c2caa06bf63c3525
John Stassen <john.stassen@gmail.com>
2020-08-24 14:24:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
-# Add your code here
+class Dog
+  @@all = []
+
+  def initialize
+    self.save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def print_all
+    @all.each do |dog|
+      puts dog
+    end
+  end
+
+  def save
+    @@all << self
